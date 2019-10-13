77ec5ce66e86b554b90837dad64818284bc441ff
vitaliishapovalov <kupper133@gmail.com>
2019-10-13 15:14:36 +0300

    Теория. Объект JavaScript Window. Часть 2

--- WebForMySelf/JavaScript/№13 Obyekt JavaScript Window. Chast 2/js/scriptW1.js
+++ WebForMySelf/JavaScript/№13 Obyekt JavaScript Window. Chast 2/js/scriptW1.js
@@ -1,7 +1,8 @@
 ///alert(window.name);
 
-
+//TODO Изменим своство переменной myvar родительского окна, через обращение к свойству opener, обьекта window
 function editMyVar() {
+	//TODO вернет измененное значение переменной myVar
 	return window.opener.myVar = 50;
 }
 
