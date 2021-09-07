e6b3284d626e9e5c5e3e41a6b6e86a78084c8a2c
vshapovalov <kupper133@gmail.com>
2021-09-07 15:51:02 +0300

    refactor(project): corrected folder and script for all code

---
+++ Variables/script.js
@@ -0,0 +1,38 @@
+var first_var = 1;
+var second = 2;
+
+// var res = first_var + second;
+
+var res = 'Маша забивае ' + 20 + ' мячей';
+
+//В джсе типы данные
+//String 'hello', "world"
+//bool true false
+//number 10,20
+
+//null
+//true false
+
+alert(res);
+
+var my_var = 10;
+
+var a = 10;
+
+var b = 10;
+var c = '20';
+
+/*10, 20
+'hello',"hello"
+true false
+
+null
+undefined*/
+
+c = 45;
+a = c + b; // 'hello' + 'world ' = 'helloworld'
+alert(a);
+
+
+
+//stringdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfg
